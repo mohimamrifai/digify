cd14f5d1897cb1734553c543b5ee9117c45fc4e7
mohimamrifai <imamrifaibisnis@gmail.com>
2024-07-22 00:10:49 +0700

    update

--- src/components/section-two.tsx
+++ src/components/section-two.tsx
@@ -4,26 +4,26 @@ import Image from "next/image";
 
 export default function SectionTwo() {
   return (
-    <div data-aos="fade-up" data-aos-duration="1000" className="flex flex-col space-y-3 md:space-y-8 bg-blue-500 mx-auto rounded-lg md:py-20 w-11/12 px-2 py-6">
-      <h2 className="text-2xl font-semibold md:text-2xl md:mx-auto md:text-center md:w-8/12 text-white lg:text-3xl">
+    <div data-aos="fade-up" data-aos-duration="1000" className="flex flex-col space-y-3 md:space-y-8 bg-blue-500 mx-auto rounded-lg md:py-20 w-11/12 py-6 px-4">
+      <h2 className="text-xl font-semibold md:text-2xl md:mx-auto md:text-center md:w-8/12 text-white lg:text-3xl">
         Actual instances of how we have assisted companies in reaching their
         marketing goals.
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-4 md:gap-5 gap-2 md:w-10/12 md:mx-auto">
-        <div className="px-2 py-1 text-sm border-2 rounded-full text-center font-semibold text-white cursor-pointer hover:bg-orange-400 transition-all duration-200">
+        <div className="px-2 py-1 text-xs md:text-sm border-2 rounded-full text-center font-semibold text-white cursor-pointer hover:bg-orange-400 transition-all duration-200">
           All Works [20]
         </div>
-        <div className="px-2 py-1 text-sm border-2 rounded-full text-center  text-white font-semibold cursor-pointer hover:bg-orange-400 transition-all duration-200">
+        <div className="px-2 py-1 text-xs md:text-sm border-2 rounded-full text-center  text-white font-semibold cursor-pointer hover:bg-orange-400 transition-all duration-200">
           Digital Marketing [5]
         </div>
-        <div className="px-2 py-1 text-sm border-2 rounded-full text-center font-semibold text-white cursor-pointer hover:bg-orange-400 transition-all duration-200">
+        <div className="px-2 py-1 text-xs md:text-sm border-2 rounded-full text-center font-semibold text-white cursor-pointer hover:bg-orange-400 transition-all duration-200">
           UI / UX [10]
         </div>
-        <div className="px-2 py-1 text-sm border-2 rounded-full text-center font-semibold text-white cursor-pointer hover:bg-orange-400 transition-all duration-200">
+        <div className="px-2 py-1 text-xs md:text-sm border-2 rounded-full text-center font-semibold text-white cursor-pointer hover:bg-orange-400 transition-all duration-200">
           Branding [5]
         </div>
       </div>
-      <div className="flex py-5 space-x-3 md:space-x-10 overflow-hidden lg:justify-between lg:ps-24">
+      <div className="flex flex-col md:flex-row py-5 space-x-3 md:space-x-10 overflow-hidden lg:justify-between lg:ps-24">
         <div>
           <div className="md:p-24 p-10 border-4 border-gray-500 relative rounded-full overflow-hidden">
             <div className="bg-orange-400 w-[100px] h-[100px] rounded-full flex items-center justify-center text-white text-sm relative z-10">
